b2fa395ae85402c71c3d41a8f537aed74ef1d4ab
Belinda Teh <belinda.teh@tri.global>
2018-07-03 14:52:11 -0700

    Add url for modifying peace state

--- docker/server.js
+++ docker/server.js
@@ -6,12 +6,27 @@ const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
+// Keep track of servo peace state
+let peaceful = false;
+
 server.listen(3001, function() {
   console.log('listening on *:3001');
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello world<br/>\nPeaceful: ' + peaceful);
+});
+
+app.post('/peace', (req, res) => {
+  if (req.query.peace == 1) {
+    peaceful = true;
+    res.sendStatus(200);
+  } else if (req.query.peace == 0) {
+    peaceful = false;
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 io.on('connection', function(socket) {
