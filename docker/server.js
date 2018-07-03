e7ebc28118087f1a4dc563af35a1faa84581ef99
Belinda Teh <belinda.teh@tri.global>
2018-07-03 00:15:40 -0700

    Version fixes

--- docker/server.js
+++ docker/server.js
@@ -3,8 +3,12 @@ const http = require('http');
 const socketio = require('socket.io');
 
 const app = express();
-const server = http.createServer(app);
-const io = socketio(http);
+const server = http.Server(app);
+const io = socketio(server);
+
+server.listen(3001, function() {
+  console.log('listening on *:3001');
+});
 
 app.get('/', (req, res) => {
   res.send('Hello world\n');
@@ -12,10 +16,6 @@ app.get('/', (req, res) => {
 
 io.on('connection', function(socket) {
   socket.on('servo-peace', function(msg) {
-    io.emit('servo-peace', msg);
+     console.log('got servo peace', msg);
   });
-});
-
-server.listen(3001, function() {
-  console.log('listening on *:3001');
 });
